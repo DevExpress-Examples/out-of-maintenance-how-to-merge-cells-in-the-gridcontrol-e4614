29ef882807f966ba8c36e699c4a6bce28b57fd5b
DevExpressExampleBot <fakeEmail>
2022-12-17 08:25:29 +0400

    Source auto update [skip ci]

--- VB/WpfApplication88/MainWindow.xaml.vb
+++ VB/WpfApplication88/MainWindow.xaml.vb
@@ -1,44 +1,34 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WpfApplication88
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-
-            Dim customers As New ObservableCollection(Of Product)()
+            Me.InitializeComponent()
+            Dim customers As ObservableCollection(Of Product) = New ObservableCollection(Of Product)()
             customers.Add(New Product() With {.ID = 0, .Name = "Product1", .Category = "Category1"})
             customers.Add(New Product() With {.ID = 1, .Name = "Product2", .Category = "Category1"})
             customers.Add(New Product() With {.ID = 2, .Name = "Product3", .Category = "Category2"})
             customers.Add(New Product() With {.ID = 3, .Name = "Product4", .Category = "Category2"})
             customers.Add(New Product() With {.ID = 4, .Name = "Product5", .Category = "Category1"})
             customers.Add(New Product() With {.ID = 5, .Name = "Product6", .Category = "Category3"})
-            grid.ItemsSource = customers
+            Me.grid.ItemsSource = customers
         End Sub
     End Class
 
     Public Class Product
-        Public Property ID() As Integer
 
-        Public Property Name() As String
-        Public Property Category() As String
+        Public Property ID As Integer
+
+        Public Property Name As String
+
+        Public Property Category As String
     End Class
 End Namespace
